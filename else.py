c6b9f73317c8e0f862fc3ba01afa9416c3c5de46
glVerdi <gabrielVerdi12@gmail.com>
2022-08-23 10:49:11 -0300

    Adicionando mais exercicios

---
+++ else.py
@@ -0,0 +1,40 @@
+x = 4
+if x == 5:
+    print('Yes')
+else:
+    print('No')
+
+if 1 + 1 == 2:
+    if 2 * 2 == 8:
+        print('if')
+    else:
+        print('else')
+
+num = 3
+if num == 1:
+    print('One')
+else:
+    if num == 2:
+        print('Two')
+    else:
+        if num == 3:
+            print('Three')
+        else:
+            print('Something else')
+
+x = 10
+y = 20
+if x > y:
+    print('if')
+else:
+    print('else')
+
+num = 3
+if num == 1:
+    print('One')
+elif num == 2:
+    print('Two')
+elif num == 3:
+    print('Three')
+else:
+    print('Something else')
---
+++ if.py
@@ -0,0 +1,22 @@
+if 10 > 5:
+    print('10 greater than 5')
+print('Program ended')
+
+spam = 7
+if spam > 5:
+    print('five')
+if spam > 8:
+    print('eight')
+num = 12
+if num > 5:
+    print('Bigger than 5')
+    if num <= 47:
+        print('Between 5 and 47')
+
+num = 7
+if num > 3:
+    print('3')
+    if num < 5:
+        print('5')
+        if num == 7:
+            print('7')
